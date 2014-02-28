9bf5d29f16df37757478cdf1308aef457daca4ea
Macgill <macgill@wr-130-64-93-139.medford.tufts.edu>
2014-02-27 19:57:11 -0500

    separated cockroach from characters.py, cockroach not fully functional yet

--- characters.py
+++ characters.py
@@ -1,4 +1,6 @@
-import pygame
+import pygame, cockroach
+
+#make separate cockroach class, decrement health for hero collision, make cockroach pace platform while hero no on platform
 
 class Hero(pygame.sprite.Sprite):
         def __init__(self):
@@ -118,67 +120,4 @@ class Hero(pygame.sprite.Sprite):
                                 if (self.rect.x > platform.rect.x):
                                         self.rect.x = rect.x + col.width
                                 self.moveVector[0] = 0
-                if not grounded:
-                        self.inAir = True
-
-class Cockroach(pygame.sprite.Sprite):
-        def __init__(self, x, y):
-                pygame.sprite.Sprite.__init__(self)
-                
-                try:
-                        self.image = pygame.image.load('cockroach.png').convert()
-                except:
-                        self.image = pygame.Surface((100, 63))
-                self.rect = self.image.get_rect().move(x, y)
-                self.moveVector = [0, 0]
-                self.inAir = True
-                self.grounded = False
-
-        def update(self, hero, platforms):
-                #need to find a way to know if hero is on platform
-                #this moves towards hero
-                self.collide(platforms, None)
-                if (self.grounded == True):
-                        if (self.rect.x < hero.rect.x):
-                                self.rect.x += 2
-                        if (self.rect.x > hero.rect.x):
-                                self.rect.x -= 2
-                #this moves downward 
-                if (self.inAir == True):
-                        self.rect.y += 4 # 
-                        
-                
-        def draw(self, screen, world):
-                screen.blit(self.image, self.rect.move(world))
-
-        def entityCollide(self, who):
-                pass
-                
-        def collide(self, platforms, dynamics):
-                self.platformCollide(platforms)
-                #do nothing with dynamics for now
-
-        def platformCollide(self, platforms):
-                for platform in platforms:
-                        rect = self.rect.move((0, self.moveVector[1]))
-                        col = platform.collides(rect) 
-                        if col is not None:
-                                if (self.rect.y > platform.rect.y):
-                                        self.rect.y = rect.y + col.height
-                                        self.moveVector[1] = 0
-
-                                if (self.rect.y < platform.rect.y):
-                                        self.inAir = False
-                                        self.grounded = True
-                                        self.rect.y = rect.y - col.height
-                                        self.moveVector[1] = 0
-                        rect = self.rect.move((self.moveVector[0], 0))
-                        col = platform.collides(rect)
-                        if col is not None:
-                                if (self.rect.x < platform.rect.x):
-                                        self.rect.x = rect.x - col.width
-                                if (self.rect.x > platform.rect.x):
-                                        self.rect.x = rect.x + col.width
-                                self.moveVector[0] = 0
-
 
