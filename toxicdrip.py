e8a38cec825e768555d2a00f0b23a9e7e3991a76
Macgill <macgill@resnet133-243.medford.tufts.edu>
2014-03-23 22:06:15 -0400

    working toxic drip
    Former-commit-id: 692e483f6eb7d1eda1fd9be3e21c178f0bc088de

---
+++ toxicdrip.py
@@ -0,0 +1,96 @@
+import pygame, os, sys, characters, pyganim
+from pygame.locals import *
+
+class Toxicdrip(pygame.sprite.Sprite):
+
+    def load_image(self, image_name):
+        try:
+            image = pygame.image.load(image_name)
+        except pygame.error, message:
+            print"Cannot load image " + image_name
+            raise SystemExit, message
+        return image.convert_alpha()
+    
+    def __init__(self, x, y, ground, dy):
+        pygame.sprite.Sprite.__init__(self)
+
+        #load image for now we will use a rectangle
+        self.image = self.load_image('assets/drop.jpg')
+        self.rect = self.image.get_rect().move(x, y)
+
+        self.dy = dy
+        #ground is the y coordinate that the dorp should splatter
+        #at if it doesn't hit player before
+        self.ground = ground
+
+        self.splatted = False
+
+
+    def update(self, hero):
+        #check to see if drop has either hit a platform or a the player
+        if self.rect.colliderect(hero.rect):
+            self.hitHero(hero)
+
+        if self.rect.collidepoint(self.rect.x, self.ground):
+            self.hitPlatform()
+
+        self.rect.y = self.rect.y + self.dy
+            
+    def hitHero(self, hero):
+        #if the drop hits the player it needs to hurt him
+        hero.health = hero.health - 10
+        self.splatted = True
+        pass
+
+    def hitPlatform(self):
+        self.image = self.load_image('assets/splat.gif')
+        self.dy = 0
+        self.splatted = True
+
+    def draw(self, screen):
+        draw_pos = self.rect
+        screen.blit(self.image, draw_pos)
+
+if __name__ == "__main__":
+
+    #Constants
+    FPS = 50
+    SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+    BACKGROUND_COLOR = (255, 255, 255)
+
+    #init pygame
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+    pygame.display.set_caption('toxic drop demo')
+    clock = pygame.time.Clock()
+
+    drips = []
+    drip = Toxicdrip(300, -40, 500, 10)
+    drips.append(drip)
+
+    hero = characters.Hero()
+    #Game loop
+    while True:
+        time_passed = clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                       pygame.quit()
+                       sys.exit()
+
+        screen.fill(BACKGROUND_COLOR)
+
+        for drop in drips:
+                drop.update(hero)
+                drop.draw(screen)
+                if drop.splatted == True:
+                    drips.remove(drop)
+                    drop.kill()
+                    drips.append(Toxicdrip(300, -40, 500, 10))
+
+        pygame.display.update()
+
+        
