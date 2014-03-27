e23cc0f5f06f0d2b60da0ac5d5ca7717e94477ba
Arthur <arthur@aberman.(none)>
2014-03-27 17:16:38 -0400

    fixed joystick on opening cutscene

--- game.py
+++ game.py
@@ -220,7 +220,7 @@ pygame.display.flip()
 while intro:
         screen.fill(16777215)
         for event in pygame.event.get():
-                if event.type == KEYDOWN:
+                if event.type == KEYDOWN | event.type == JOYBUTTONDOWN:
                         intro = False
                         pygame.mixer.music.stop()
                         menu.stop()
@@ -254,7 +254,7 @@ while scene:
         pygame.display.flip()
         for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
-                elif event.type == KEYDOWN:
+                elif event.type == KEYDOWN | event.type == JOYBUTTONDOWN:
                     scene = False
                     pygame.mixer.music.stop()
         fpsClock.tick(30)
